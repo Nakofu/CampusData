ebb4050640194a8c86410534448b64740d5d3048
Ilya Sychkov <Sychqh@gmail.com>
2024-01-22 18:29:45 +0500

    Delete comments

--- Campus/Controllers/CampusController.cs
+++ Campus/Controllers/CampusController.cs
@@ -9,47 +9,6 @@ namespace Campus.Controllers;
 [ApiController]
 public class CampusController : ControllerBase
 {
-    /*
-    private readonly ApplicationsContext _applicationsContext;
-    private readonly AttendanceMobileContext _attendanceMobileContext;
-    private readonly AttendanceWebContext _attendanceWebContext;
-    private readonly FundBonusContext _fundBonusContext;
-    private readonly FundPrivilegeContext _fundPrivilegeContext;
-    private readonly FundStipendContext _fundStipendContext;
-    private readonly HonoursContext _honoursContext;
-    private readonly MajorsContext _majorsContext;
-    private readonly PagesContext _pagesContext;
-    private readonly ParticipantsContext _participantsContext;
-    private readonly RegionsContext _regionsContext;
-    private readonly SchoolsContext _schoolsContext;
-    
-    public CampusController(ParticipantsContext participantsContext, 
-        ApplicationsContext applicationsContext, 
-        AttendanceMobileContext attendanceMobileContext, 
-        AttendanceWebContext attendanceWebContext, 
-        FundBonusContext fundBonusContext, 
-        FundPrivilegeContext fundPrivilegeContext, 
-        FundStipendContext fundStipendContext, 
-        HonoursContext honoursContext, 
-        MajorsContext majorsContext, 
-        PagesContext pagesContext, 
-        RegionsContext regionsContext, 
-        SchoolsContext schoolsContext)
-    {
-        _participantsContext = participantsContext;
-        _applicationsContext = applicationsContext;
-        _attendanceMobileContext = attendanceMobileContext;
-        _attendanceWebContext = attendanceWebContext;
-        _fundBonusContext = fundBonusContext;
-        _fundPrivilegeContext = fundPrivilegeContext;
-        _fundStipendContext = fundStipendContext;
-        _honoursContext = honoursContext;
-        _majorsContext = majorsContext;
-        _pagesContext = pagesContext;
-        _regionsContext = regionsContext;
-        _schoolsContext = schoolsContext;
-    }
-*/
     [EnableCors("AllowAny")]
     [HttpGet("/getInfo")]
     public async Task<string> GetStuff()
