244ec7d9e2cec83c3e574286e32e60b6af09cf42
Ilya Sychkov <Sychqh@gmail.com>
2024-01-22 17:24:57 +0500

    Tests

--- Campus/Data/ParticipantsData.cs
+++ Campus/Data/ParticipantsData.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Net.Mime;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using Campus.Models;
 using Newtonsoft.Json;
@@ -9,7 +10,7 @@ public class ParticipantsData
 {
     public static IEnumerable<Participants> Aa()
     {
-        using var r = new StreamReader("/Data/participants.json");
+        using var r = new StreamReader(Directory.GetCurrentDirectory() + "/participants.json");
         
         var onj = JsonConvert.DeserializeObject<List<Participants>>(r.ReadToEnd());
         return onj;
